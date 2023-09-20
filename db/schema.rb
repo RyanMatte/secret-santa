c4764f4b05cde7d72d4e26463a69d4a8f5d41ab5
Ryan Matte <ryanmatte55@gmail.com>
2023-09-19 20:28:14 -0400

    add some more stufff'

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_005147) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_021922) do
   create_table "gifts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "url"
+    t.string "notes"
   end
 
   create_table "users", force: :cascade do |t|
